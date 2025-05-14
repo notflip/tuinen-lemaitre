a19c51f2f7dc751b59fb3eeb7d8edd6f68668d78
Miguel Stevens <miguel@studiomonty.be>
2025-05-14 20:15:10 +0200

    CardsComponent

--- src/blocks/Cards/CardsComponent.tsx
+++ src/blocks/Cards/CardsComponent.tsx
@@ -11,14 +11,14 @@ export const CardsComponent: React.FC<Cards> = (props) => {
       <div className="max-w-xl mb-12">
         <h3>{title}</h3>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         {(items || []).map((item, index) => (
           <div key={index} className="bg-highlight p-4 lg:p-8 rounded-lg">
             {item.icon && (
               <DynamicIcon iconName={item.icon} size={42} className="text-primary-500" />
             )}
             <h4 className="my-8">{item.title}</h4>
-            <p className="mb-8 text-lg">{item.text}</p>
+            <p className="mb-8 max-w-md text-lg">{item.text}</p>
             {item.link.type !== "none" && <Type4 variant="light" link={item.link} />}
           </div>
         ))}
