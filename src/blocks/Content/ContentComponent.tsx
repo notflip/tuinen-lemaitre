6a452301f3b36ff1331c75a0ed847faff7ec3a5f
Miguel Stevens <miguel@studiomonty.be>
2025-05-16 15:30:38 +0200

    Removed bgColor again

--- src/blocks/Content/ContentComponent.tsx
+++ src/blocks/Content/ContentComponent.tsx
@@ -3,10 +3,10 @@ import RichText from "@/components/rich-text"
 import { BlockContainer } from "@/blocks/BlockContainer"
 
 export const ContentComponent: React.FC<Content> = (props) => {
-  const { content, bgColor } = props
+  const { content } = props
 
   return (
-    <BlockContainer bgColor={bgColor} {...props}>
+    <BlockContainer {...props}>
       <div className="py-24 lg:py-32">{content && <RichText data={content} />}</div>
     </BlockContainer>
   )
