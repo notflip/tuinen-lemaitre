7f79ca38ea95f1367e78f56c9cc16dd11c2b7903
Miguel Stevens <miguel@studiomonty.be>
2025-05-16 10:47:41 +0200

    Removed migrations

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250516_070125 from './20250516_070125';
-
-export const migrations = [
-  {
-    up: migration_20250516_070125.up,
-    down: migration_20250516_070125.down,
-    name: '20250516_070125'
-  },
-];
