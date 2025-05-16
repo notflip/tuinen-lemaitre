1de8d423c8d3f25b68799e4908103bd173d98fdf
Miguel Stevens <miguel@studiomonty.be>
2025-05-16 17:12:54 +0200

    Added Mail icon

--- src/blocks/Content/ContentComponent.tsx
+++ src/blocks/Content/ContentComponent.tsx
@@ -7,7 +7,7 @@ export const ContentComponent: React.FC<Content> = (props) => {
 
   return (
     <BlockContainer bgColor={bgColor} {...props}>
-      <div className="py-24 lg:py-32">{content && <RichText data={content} />}</div>
+      <div className={`${bgColor ? "py-24 lg:py-32" : ""}`}>{content && <RichText data={content} />}</div>
     </BlockContainer>
   )
 }
--- src/components/social-media-icon.tsx
+++ src/components/social-media-icon.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link"
 import {
+  FaEnvelope,
+  FaEnvelopeOpen,
   FaFacebook,
   FaGlobe,
   FaInstagram,
@@ -9,6 +11,8 @@ import {
   FaYoutube,
 } from "react-icons/fa6"
 import { IconType } from "react-icons"
+
+import { TbMail } from "react-icons/tb"
 
 interface IconMapping {
   Icon: IconType
@@ -26,6 +30,12 @@ const iconMap: IconMapping[] = [
 ]
 
 const getSocialIcon = (url: string, size: number) => {
+  // Check if url is an email address
+  const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(url)
+  if (isEmail) {
+    return <TbMail size={size} />
+  }
+
   const mapping = iconMap.find(({ domain }) => url.includes(domain))
   const IconComponent = mapping ? mapping.Icon : FaGlobe
   return <IconComponent size={size} />
@@ -36,16 +46,22 @@ interface SocialMediaIconProps {
   url: string
 }
 
-const SocialMediaIcon: React.FC<SocialMediaIconProps> = ({ url, size = 20 }) => (
-  <Link
-    aria-label="Social link"
-    href={url}
-    target="_blank"
-    rel="noopener noreferrer"
-    className="block rounded-full p-4 bg-primary-300 hover:bg-primary-500 text-foreground!"
-  >
-    {getSocialIcon(url, size)}
-  </Link>
-)
+const SocialMediaIcon: React.FC<SocialMediaIconProps> = ({ url, size = 20 }) => {
+  const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(url)
+  const href = isEmail ? `mailto:${url}` : url
+  return (
+    <Link
+      aria-label="Social link"
+      href={href}
+      {...(!isEmail && {
+        target: "_blank",
+        rel: "noopener noreferrer",
+      })}
+      className="block rounded-full p-4 bg-primary-300 hover:bg-primary-500 text-foreground!"
+    >
+      {getSocialIcon(url, size)}
+    </Link>
+  )
+}
 
 export default SocialMediaIcon
