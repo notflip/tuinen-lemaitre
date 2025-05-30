5b583fae426a8a445066829bfe41edc7f7bf1897
Miguel Stevens <miguel@studiomonty.be>
2025-05-30 07:53:35 +0200

    Added migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20250516_130354 from './20250516_130354';
 import * as migration_20250516_134203 from './20250516_134203';
 import * as migration_20250526_121330 from './20250526_121330';
+import * as migration_20250530_055327 from './20250530_055327';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20250526_121330.up,
     down: migration_20250526_121330.down,
-    name: '20250526_121330'
+    name: '20250526_121330',
+  },
+  {
+    up: migration_20250530_055327.up,
+    down: migration_20250530_055327.down,
+    name: '20250530_055327'
   },
 ];
